c9e305c27da4a0faf593cbf1b0175f2c86a9bbb8
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2023-08-17 23:35:37 +0100

    Add github action to deploy the docs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://wildflowai.github.io",
+  base: "/docs",
+
   integrations: [
     starlight({
       title: "wildflow docs 🐳",
